0967b962fd9f3c668a5661515670480b35ffe82b
Rafael <rafael@wheelhouse.be>
2019-08-07 11:44:49 +0200

    Use basic postgres + support async await with Babel

--- api.js
+++ api.js
@@ -1,7 +1,17 @@
 import express from 'express';
 import path from 'path';
+import { Pool } from 'pg';
 
 const app = express();
+const pool = new Pool({
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'WheelhouseLibrary',
+  password: '',
+  port: 54320,
+});
+
+const getBooks = () => pool.query('SELECT * FROM "Library"."Book"');
 
 // Enable JSON request body
 app.use(express.json());
@@ -10,7 +20,16 @@ app.use(express.json());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // Endpoints
-app.get('/', (req, res) => res.status(200).send({ message: 'YAY! Congratulations! Your first endpoint is working' }));
+app.get('/', (req, res) => res.status(200).send({ message: 'Wheelhouse Library API' }));
+
+app.get('/api/v1/books', async (req, res) => {
+  const books = await getBooks();
+
+  console.log(books);
+  res.json({
+    status: 'ok',
+  });
+});
 
 // Listen on port 3000
 app.listen(3000);
