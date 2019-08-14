75f572d478357b3f1d811e0ac4355c5d51c00e1a
Rafael <rafael@wheelhouse.be>
2019-08-14 13:11:19 +0200

    ESLint fixes

--- api.js
+++ api.js
@@ -1,29 +1,29 @@
 import express from 'express';
 import path from 'path';
-import {Pool} from 'pg';
+import { Pool } from 'pg';
 import cors from 'cors';
 
-import {getAllBooks} from './models/book';
-import {getAllUsers, upsertUsers} from "./models/user";
-
 import dotenv from 'dotenv';
-import {getUsers} from "./util/slack_users";
+import { getAllBooks } from './models/book';
+import { getAllUsers, upsertUsers } from './models/user';
+
+import { getUsers } from './util/slack_users';
 
 dotenv.config();
 
-const corsOptions = {
-    origin: 'http://localhost:1234',
-};
+// const corsOptions = {
+//   origin: 'http://localhost:1234',
+// };
 
 const app = express();
 
 // Make a pool-based connection to Postgres
 const pool = new Pool({
-    user: 'postgres',
-    host: '127.0.0.1',
-    database: 'postgres',
-    password: '',
-    port: 54320,
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'postgres',
+  password: '',
+  port: 54320,
 });
 
 // Enable JSON request body usage
@@ -39,35 +39,35 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 // Endpoints
 // ---------
 app.get('/',
-    (req, res) => res.status(200).send({message: 'Wheelhouse Library API'}));
+  (req, res) => res.status(200).send({ message: 'Wheelhouse Library API' }));
 
 // Get all books
 app.get('/api/v1/books',
-    async (req, res) => {
-        const {rows} = await getAllBooks(pool);
-        res.json(rows);
-    });
+  async (req, res) => {
+    const { rows } = await getAllBooks(pool);
+    res.json(rows);
+  });
 
 // Get all books
 app.get('/api/v1/users',
-    async (req, res) => {
-        const {rows} = await getAllUsers(pool);
-        res.json(rows);
-    });
+  async (req, res) => {
+    const { rows } = await getAllUsers(pool);
+    res.json(rows);
+  });
 
 // Authorize via Slack
 app.get('/auth', cors(), (req, res) => {
-    console.log(req.body);
-    res.status(200).send();
+  console.log(req.body);
+  res.status(200).send();
 });
 
 // Listen on port 3000
 app.listen(3000);
 
 getUsers().then(
-    users => {
-        upsertUsers(pool, users);
-    }
+  (users) => {
+    upsertUsers(pool, users);
+  },
 );
 
 console.log('Running Wheelhouse Library RESTful API @ PORT:3000'); // eslint-disable-line
