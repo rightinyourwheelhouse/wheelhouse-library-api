a93006bdef101c3fa01d1c36c2c0f91526f7cf73
Roel <roel@wheelhouse.be>
2019-08-14 15:39:30 +0200

    updated jsdoc

--- util/db-utils.js
+++ util/db-utils.js
@@ -1,14 +1,12 @@
-
 /**
  * Upsert users
  * @param {Connection} pool - the pg pool
  * @param {string} tableName - the name of the table to upsert into
- * @param {Array<{}>} inserts - the inserts to upsert
+ * @param {Array<Object>} inserts - the objects to upsert into the table
  */
 export function upsert(inserts, tableName, pool) {
     inserts.map(insert =>
-        `
-        INSERT INTO  "Library"."${tableName}" as tbl (${Object.keys(insert).join(', ')})
+        `INSERT INTO  "Library"."${tableName}" as tbl (${Object.keys(insert).join(', ')})
         VALUES
            (${Object.values(insert).map(value => `'${value}'`).join(', ')}) 
         ON CONFLICT  ON CONSTRAINT "${tableName}_id_key" 
