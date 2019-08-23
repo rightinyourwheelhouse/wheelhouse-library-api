817199700778a7051b70a2f8545b51aa07ecadcb
Roel <roel@wheelhouse.be>
2019-08-23 15:08:54 +0200

    8 books

--- util/book-seed.js
+++ util/book-seed.js
@@ -18,5 +18,59 @@ export function getBooks() {
             rating:4.5,
             coverImg:"https://images-na.ssl-images-amazon.com/images/I/41BKx1AxQWL._SX396_BO1,204,203,200_.jpg"
         },
+        {
+            id:"7cddc055-fc35-4dd6-9318-1480d5a86a97",
+            title:"Head first Design patterns",
+            author:"Eric Freeman, Bert Bates, Kathy Sierra, Elisabeth Robson",
+            ISBN:"978-0596007126",
+            pages:"694",
+            rating:4.3,
+            coverImg:"https://images-na.ssl-images-amazon.com/images/I/61ZG-hATOeL._SX430_BO1,204,203,200_.jpg"
+        },
+        {
+            id:"bb0aff9e-43a8-4c58-aaa6-caa7d3aea968",
+            title:"Building microservices",
+            author:"Sam Newman",
+            ISBN:"978-1491950357",
+            pages:"280",
+            rating:4,
+            coverImg:"https://images-na.ssl-images-amazon.com/images/I/51m85J4Zi9L._SX378_BO1,204,203,200_.jpg"
+        },
+        {
+            id:"c004036e-aed9-4684-8e30-0e813f9cbc2c",
+            title:"Test Driven Development",
+            author:"Kent Beck",
+            ISBN:"978-0321146533",
+            pages:"240",
+            rating:4,
+            coverImg:"https://images-na.ssl-images-amazon.com/images/I/51kDbV%2BN65L._SX396_BO1,204,203,200_.jpg"
+        },
+        {
+            id:"0f458c08-9d8a-492a-b093-99782a527f25",
+            title:"Refactoring",
+            author:"Martin Fowler",
+            ISBN:"978-0134757599",
+            pages:"448",
+            rating:4,
+            coverImg:"https://images-na.ssl-images-amazon.com/images/I/41LBzpPXCOL._SX379_BO1,204,203,200_.jpg"
+        },
+        {
+            id:"eb3cff0e-23a3-4138-b764-6de76cf6b9c0",
+            title:"The Art of Scalability",
+            author:"Martin L. Abbott, Michael T. Fisher",
+            ISBN:"978-0134032801",
+            pages:"624",
+            rating:4,
+            coverImg:"https://images-na.ssl-images-amazon.com/images/I/51TU%2BztIcbL._SX382_BO1,204,203,200_.jpg"
+        },
+        {
+            id:"63453500-8ac3-454a-91e6-568fec562592",
+            title:"Don''t make me think",
+            author:"Steve Krug",
+            ISBN:"978-0321965516",
+            pages:"216",
+            rating:4.3,
+            coverImg:"https://images-na.ssl-images-amazon.com/images/I/51pnouuPO5L._SX387_BO1,204,203,200_.jpg"
+        }
     ]
 }
