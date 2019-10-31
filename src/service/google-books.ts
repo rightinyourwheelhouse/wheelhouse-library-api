2838884abc9f267a0f9513a78d8c952bc8d63264
Roel <roel@wheelhouse.be>
2019-10-31 14:45:39 +0100

    key instead of api

--- src/service/google-books.ts
+++ src/service/google-books.ts
@@ -2,7 +2,7 @@ import request from "request-promise";
 
 export function getGoogleBooksMetaData(book) {
     return request({
-        uri: `https://www.googleapis.com/books/v1/volumes?q=+isbn:${book.ISBN}&api=${process.env.GOOGLE_API_KEY}`,
+        uri: `https://www.googleapis.com/books/v1/volumes?q=+isbn:${book.ISBN}&key=${process.env.GOOGLE_API_KEY}`,
         json: true,
     }).then((googleBooks) => googleBooks.items ? googleBooks.items[0].volumeInfo : null);
 }
