c2e348390ec8fbc9eaf7abe3e725aee7ca5008d0
Roel <roel@wheelhouse.be>
2019-10-31 14:39:27 +0100

    forgot api prefix

--- src/api/login-controller.ts
+++ src/api/login-controller.ts
@@ -3,10 +3,10 @@ export function loginController(app, passport) {
     app.use(passport.initialize());
 
     // path to start the OAuth flow
-    app.get("/auth/slack", passport.authorize("slack"));
+    app.get("/api/v1/auth/slack", passport.authorize("slack"));
 
     // OAuth callback url
-    app.get("/auth/slack/callback",
+    app.get("/api/v1/auth/slack/callback",
         passport.authenticate("slack", { failureRedirect: "/login" }), // TODO: add failureRedirects
         (req, res) => {
             console.info(req.session);
