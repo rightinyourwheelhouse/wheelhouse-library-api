0dd7e015bd5ce5dbe10c4926ecc3448acbe575e4
Roel <roel@wheelhouse.be>
2019-10-31 14:08:55 +0100

    Added logging to user controller

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,8 @@
 import {upsert} from "../util/db-utils";
+import {log} from "../util/debug-logger";
 
 export function getAllUsers(pool) {
+    log(`Getting all users`);
     return pool.query('SELECT * FROM "Library"."Account"');
 }
 
@@ -10,10 +12,12 @@ export function getAllUsers(pool) {
  * @param {Array<{id,username,avatar}>} users - the users to upsert
  */
 export function upsertUsers(pool, users) {
+    log(`Upserting user ${JSON.stringify(users, null, 4)} `);
     return upsert(users, "Account", pool);
 }
 
 export function upsertUser(pool, user: User) {
+    log(`Upserting user ${JSON.stringify(user, null, 4)} `);
     return upsert([user], "Account", pool);
 }
 
