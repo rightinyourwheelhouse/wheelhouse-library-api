448ce364b397e8eb03360273c7b13ac2411ff3c6
Roel <roel@wheelhouse.be>
2020-01-03 13:19:46 +0100

    Added redirect uri

--- src/api/login-controller.ts
+++ src/api/login-controller.ts
@@ -1,15 +1,20 @@
 import * as core from "express-serve-static-core";
 import request from "request-promise";
 import {getUser, upsertUser} from "../models/user";
-import {log} from "../util/debug-logger";
 
 export function loginController(app: core.Express, pool) {
     // path to start the OAuth flow
-    const {SLACK_CLIENT_ID, SLACK_CLIENT_SECRET} = process.env;
+    const {SLACK_CLIENT_ID, SLACK_CLIENT_SECRET, SLACK_CALLBACK_URL} = process.env;
     app.get("/api/v1/auth/slack",
         async (req, res) => {
+            const queryParams = Object.entries({
+                code: req.query.code,
+                client_secret: SLACK_CLIENT_SECRET,
+                client_id: SLACK_CLIENT_ID,
+                redirect_uri: SLACK_CALLBACK_URL,
+            }).map(([key, value]) => `${key}=${value}`);
             const response = await request({
-                uri: `https://slack.com/api/oauth.access?code=${req.query.code}&client_secret=${SLACK_CLIENT_SECRET}&client_id=${SLACK_CLIENT_ID}`,
+                uri: `https://slack.com/api/oauth.access?${queryParams.join("&")}`,
                 json: true,
             });
             if (response.ok) {
